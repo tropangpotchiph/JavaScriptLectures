183266517d53bfd902e4adc1c2f421026f18f846
Ryan Azur <kodegowd64@gmail.com>
2023-04-27 15:08:27 +0800

    Initial Commit

---
+++ 01-variable-datatypes/01console/script.js
@@ -0,0 +1,28 @@
+// alert("Hello from JS file");
+
+//log a number
+console.log(100);
+
+// log string
+console.log("Hello World");
+
+//log multiple values
+console.log(20, "Hello", true);
+
+// log a variable
+const x = 100;
+console.log(x);
+
+//console error & warning
+console.error("Alert");
+console.warn("Warning");
+
+//log object as a table
+console.table({ name: "Ryan", email: "ryan@gmail.com" });
+
+// Group Console commands
+console.group("simple");
+console.log(x);
+console.error("Alert");
+console.warn("Warning");
+console.groupEnd();
