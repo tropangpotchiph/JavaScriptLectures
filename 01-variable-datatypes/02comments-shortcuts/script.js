205c7a9e444b94d9767dd0759c7b5b4e71a465a7
Ryan Azur <kodegowd64@gmail.com>
2023-04-27 15:33:50 +0800

    Initial Commit

---
+++ 01-variable-datatypes/02comments-shortcuts/script.js
@@ -0,0 +1,9 @@
+//Single line of code comment
+
+// The shortcut key is:    ctrl + /
+
+// Multi line comment shortcut key:    Shift + Alt + A
+
+/* console.log(100);
+console.log("Hello World");
+console.log(20, "Hello", true); */
